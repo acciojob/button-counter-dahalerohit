50256aedc078c178ffd3d83c82a5a188d98dd77c
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2026-05-06 14:31:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import Counter from './src/components/counter.jsx';
 
 const App = () => {
   return (
     <div>
-      const counter = () => {
-    const [count, setcount] = useState(0);
-    const onAdd = () => {
-        setcount(count + 1);
-    };
-    return (
-        <div>
-            <p>Button Click {count} times</p>
-            <button onClick={onAdd}>Click me</button>
-        </div>
-    )
+      <Counter/>
     
     </div>
   )
